363f887fe029951a14015684cee4439ccdec5098
nlvz <smacchia.mattia@gmail.com>
2018-11-20 21:48:37 +0100

    feat: responsive workarea

--- src/components/ImageController/index.jsx
+++ src/components/ImageController/index.jsx
@@ -3,19 +3,42 @@ import { Button } from "antd";
 
 export default class ImageController extends React.Component {
     render() {
-        const { handleIncreaseWidth, handleDecreaseWidth } = this.props;
+        const {
+            handleIncreaseWidth,
+            handleDecreaseWidth,
+            handleDecreaseHeight,
+            handleIncreaseHeight
+        } = this.props;
         return (
             <div>
                 <Button
                     type="primary"
                     icon="left"
                     onClick={handleDecreaseWidth}
-                />
+                >
+                    Decrease Image Width
+                </Button>
                 <Button
                     type="primary"
                     icon="right"
                     onClick={handleIncreaseWidth}
-                />
+                >
+                    Increase Image Width
+                </Button>
+                <Button
+                    type="primary"
+                    icon="left"
+                    onClick={handleDecreaseHeight}
+                >
+                    Decrease Image Height
+                </Button>
+                <Button
+                    type="primary"
+                    icon="right"
+                    onClick={handleIncreaseHeight}
+                >
+                    Increase image Height
+                </Button>
             </div>
         );
     }
--- src/components/WorkContainer/index.jsx
+++ src/components/WorkContainer/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Col, Row } from "antd";
 
 import ImageController from "../ImageController";
 import "./style.css";
@@ -6,33 +7,54 @@ import "./style.css";
 export default class WorkContainer extends React.Component {
     state = { imgWidth: 100, imgHeight: 100 };
 
-    handleIncreaseWidth = () => {
-        const newWidth = this.state.imgWidth + 40;
-        this.setState({ imgWidth: newWidth });
+    handleDecreaseHeight = () => {
+        const newHeight = this.state.imgHeight - 40;
+        this.setState({ imgHeight: newHeight });
     };
 
     handleDecreaseWidth = () => {
         const newWidth = this.state.imgWidth - 40;
         this.setState({ imgWidth: newWidth });
     };
+
+    handleIncreaseHeight = () => {
+        const newHeight = this.state.imgHeight + 40;
+        this.setState({ imgHeight: newHeight });
+    };
+
+    handleIncreaseWidth = () => {
+        const newWidth = this.state.imgWidth + 40;
+        this.setState({ imgWidth: newWidth });
+    };
+
     render() {
         const { imgUploaded } = this.props;
         return (
-            <div className="c-WorkContainer">
-                <div className="c-WorkContainer-imgupload">
-                    <img
-                        src={imgUploaded}
-                        alt=""
-                        style={{
-                            width: this.state.imgWidth,
-                            height: this.state.imgHeight
-                        }}
-                    />
-                </div>
-                <ImageController
-                    handleIncreaseWidth={this.handleIncreaseWidth}
-                    handleDecreaseWidth={this.handleDecreaseWidth}
-                />
+            <div className="c-Create">
+                <Row>
+                    <Col span={9}>
+                        <div className="c-WorkContainer">
+                            <div className="c-WorkContainer-imgupload">
+                                <img
+                                    src={imgUploaded}
+                                    alt=""
+                                    style={{
+                                        width: this.state.imgWidth,
+                                        height: this.state.imgHeight
+                                    }}
+                                />
+                            </div>
+                        </div>
+                    </Col>
+                    <Col span={4}>
+                        <ImageController
+                            handleIncreaseWidth={this.handleIncreaseWidth}
+                            handleDecreaseWidth={this.handleDecreaseWidth}
+                            handleDecreaseHeight={this.handleDecreaseHeight}
+                            handleIncreaseHeight={this.handleIncreaseHeight}
+                        />
+                    </Col>
+                </Row>
             </div>
         );
     }
